14555709b275e90c16971478f740bb5ec86571bc
doguebart <douglasweelber.2004@outlook.com>
2024-02-02 23:44:47 -0300

    feat: ensure GetCustomersRepository gets customers from postgres

--- src/models/customer.ts
+++ src/models/customer.ts
@@ -1,12 +1,12 @@
 export interface ICustomer {
   full_name: string;
   email: string;
-  phone_number: number;
+  phone_number: string;
   password: string;
   address: {
-    zip_code: number;
+    zip_code: string;
     street: string;
-    house_number: number;
+    house_number: string;
     neighborhood: string;
     city: string;
     state: string;
--- src/repositories/customer/postgres-get-customers.ts
+++ src/repositories/customer/postgres-get-customers.ts
@@ -1,23 +1,33 @@
 import { IGetCustomersRepository } from "../../controllers/customer/get-customers/protocols";
+import { prisma } from "../../database/postgres";
 import { ICustomer } from "../../models/customer";
 
 export class PostgresGetCustomersRepository implements IGetCustomersRepository {
   async getCustomers(): Promise<ICustomer[]> {
-    return [
-      {
-        full_name: "João",
-        email: "joao.silva@email.com",
-        phone_number: 11933986562,
-        password: "senha_segura",
-        address: {
-          zip_code: 6145096,
-          street: "Rua da Miná",
-          house_number: 85,
-          neighborhood: "Jardim Conceição",
-          city: "Osaco",
-          state: "SP",
-        },
+    const customersFromDatabase = await prisma.customer.findMany({
+      include: {
+        address: true,
       },
-    ];
+    });
+
+    const customers: ICustomer[] = customersFromDatabase.map(
+      (customerFromDB) => ({
+        id: customerFromDB.id,
+        full_name: customerFromDB.full_name,
+        email: customerFromDB.email,
+        phone_number: customerFromDB.phone_number,
+        password: customerFromDB.password,
+        address: {
+          zip_code: customerFromDB.address.zip_code,
+          street: customerFromDB.address.street,
+          house_number: customerFromDB.address.house_number,
+          neighborhood: customerFromDB.address.neighborhood,
+          city: customerFromDB.address.city,
+          state: customerFromDB.address.state,
+        },
+      })
+    );
+
+    return customers;
   }
 }
