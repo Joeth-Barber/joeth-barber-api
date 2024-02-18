930ffc383c8851e71493ff298bf2f63c71fa9228
doguebart <douglasweelber.2004@outlook.com>
2024-02-18 17:42:13 -0300

    feat: create DeleteServiceController

---
+++ src/controllers/service/delete-service/delete.service.ts
@@ -0,0 +1,28 @@
+import { IService } from "../../../models/service";
+import { badRequest, ok, serverError } from "../../helpers";
+import { IController, IHttpRequest, IHttpResponse } from "../../protocols";
+import { IDeleteServiceRepository } from "./protocols";
+
+export class DeleteServiceController implements IController {
+  constructor(
+    private readonly deleteServiceRepository: IDeleteServiceRepository
+  ) {}
+
+  async handle(
+    httpRequest: IHttpRequest<any>
+  ): Promise<IHttpResponse<IService | string>> {
+    try {
+      const id = httpRequest.params?.id;
+
+      if (!id) {
+        return badRequest("Missing service Id.");
+      }
+
+      const service = await this.deleteServiceRepository.deleteService(id);
+
+      return ok<IService>(service);
+    } catch (error) {
+      return serverError();
+    }
+  }
+}
